2d9295383741485514327308bdc6a83c1c14f679
Yang-BBang <didtmdgh4629@gmail.com>
2026-04-26 23:42:48 +0900

    오타 수정

--- Python-Language/Python-Basic/Python_Basic_Code.py
+++ Python-Language/Python-Basic/Python_Basic_Code.py
@@ -181,7 +181,7 @@ else:
 temp = int(input("기온은 어때요? "))  # 사용자로부터 입력을 받는 함수, 입력받은 값은 문자열이므로 int() 함수를 사용하여 정수로 변환
 if temp >= 30:
     print("너무 더워요. 나가지 마세요")
-elif 10 <= and temp < 30:  # R: elif (10 <= temp < 30)
+elif 10 <= temp and temp < 30:  # R: elif (10 <= temp < 30)
     print("괜찮은 날씨에요")
 elif 0 <= temp < 10:
     print("외투를 챙기세요")
