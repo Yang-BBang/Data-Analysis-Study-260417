5db4a380c92b8175f665082c11e3268ab3f64fea
Yang-BBang <didtmdgh4629@gmail.com>
2026-04-26 17:09:34 +0900

    파이썬 기초(연산자 공부)

---
+++ Python-Language/Python-Basic/Python_Basic_Code.py
@@ -0,0 +1,30 @@
+# 연산자
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2
+
+print(2**3) # 2^3 = 8 (R = ^)
+print(5%3) # 나머지 구하기 = 2 (R = %%)
+print(10%3) # 나머지 구하기 = 1 (R = %%)
+print(5//3) # 나누기 몫 구하기 = 1 (R = %/%)
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True (1은 3이 아니다)
+print(not(1 != 3)) # False (1은 3이 아니다의 반대)
+print((3 > 0) and (3 < 5)) # True (3은 0보다 크고 5보다 작다)
+print((3 > 0) & (3 < 5)) # True (3은 0보다 크고 5보다)
+
+print((3 > 0) or (3 > 5)) # True (3은 0보다 크거나 5보다 크다)
+print((3 > 0) | (3 > 5)) # True (3은 0보다 크거나 5보다 크다)
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False 
