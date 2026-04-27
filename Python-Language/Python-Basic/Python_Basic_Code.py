d05ccceeb7b0caa46c1e3a2b4872f28034da044c
Yang-BBang <didtmdgh4629@gmail.com>
2026-04-27 09:33:30 +0900

    함수, 전달값과 반환값 공부 내용 정리

--- Python-Language/Python-Basic/Python_Basic_Code.py
+++ Python-Language/Python-Basic/Python_Basic_Code.py
@@ -272,3 +272,41 @@ print(students)
 students = ["Iron man", "Thor", "I am Groot"]
 students = [i.upper() for i in students]  # 학생 이름을 대문자로 변환하는 한 줄 for문
 print(students)
+
+
+
+# 7-1. 함수
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+open_account() 
+
+
+
+# 7-2. 전달값과 반환값
+def deposit(balance, money):  # 입금  # balance: 현 잔액, money: 입금액
+    print(f"입금이 완료되었습니다. 잔액은 {balance + money}원입니다.")
+    return balance + money
+
+def withdraw(balance, money):  # 출금
+    if balance >= money:  # 잔액이 출금보다 많으면
+       print(f"출금이 완료되었습니다. 잔액은 {balance - money}원 입니다.")
+       return balance - money
+    else:  # 잔액이 출금보다 적으면
+        print(f"출금이 완료되지 않았습니다. 잔액은 {balance}원 입니다.")
+        return balance
+
+def withdraw_night(balance, money):  # 저녁에 출금
+    commission = 100  # 수수료 100원
+    if balance >= money + commission:  # 잔액이 출금 + 수수료보다 많으면
+         print(f"수수료는 {commission}원이며, 잔액은 {balance - money - commission}원 입니다.")
+         return commission, balance - money - commission
+    else:  # 잔액이 출금 + 수수료보다 적으면
+        print(f"출금이 완료되지 않았습니다. 수수료는 {commission}원이며, 잔액은 {balance}원 입니다.")
+        return commission, balance
+
+balance = 0  # 현 잔액
+balance = deposit(balance, 1000)
+balance = withdraw(balance, 2000)  # 잔액이 부족하기에 불가능
+balance = withdraw(balance, 500)
+commission, balance = withdraw_night(balance, 200)
+commission, balance = withdraw_night(balance, 1000)  # 잔액이 부족하기에 불가능
