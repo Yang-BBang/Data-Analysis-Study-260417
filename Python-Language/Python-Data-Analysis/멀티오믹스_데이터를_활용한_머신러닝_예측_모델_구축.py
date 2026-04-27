91b459b70abe590d342be1fc4a4618c2d0a0073a
Yang-BBang <didtmdgh4629@gmail.com>
2026-04-27 19:28:06 +0900

    오류 수정

--- Python-Language/Python-Data-Analysis/멀티오믹스_데이터를_활용한_머신러닝_예측_모델_구축.py
+++ Python-Language/Python-Data-Analysis/멀티오믹스_데이터를_활용한_머신러닝_예측_모델_구축.py
@@ -28,3 +28,32 @@ print(f"데이터 구조: {df.shape}")
 df['Target'] = [1 if 'DIS' in idx else 0 for idx in df.index]
 
 print(df['Target'].value_counts())
+
+X = df.drop('Target', axis=1)
+y = df['Target']
+
+# 데이터 분할
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123, stratify=y
+)
+
+# 모델 생성 및 학습
+model = RandomForestClassifier(n_estimators=100, random_state=123, n_jobs=-1)
+model.fit(X_train, y_train)
+
+
+#  2) ROC 커브와 AUC
+# 모델의 분류 성능을 시각화하여 예측 성능을 평가
+# [Code 셀]
+y_prob = model.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, y_prob)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, color='red', lw=2, label=f'ROC curve (AUC = {roc_auc:.3f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc="lower right")
+plt.show()
